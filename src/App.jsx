2f62042d7ed1b860cde41249acf1820e82f08645
plumbz <plumby.eschenbacher@yahoo.com>
2025-05-29 01:10:38 +0200

    created intro.jsx, intro.css

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import Navbar from "./components/Navbar/navbar";
+import Intro from "./components/Intro/intro";
 
 function App() {
   
@@ -6,6 +7,7 @@ function App() {
     <>
       <div clasName="App">
       <Navbar/>
+      <Intro/>
       </div>
      
     </>
---
+++ src/components/Intro/intro.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import './intro.css';
+
+const intro = () => {
+    return (
+        <div>intro</div>
+    )
+}
+
+export default intro
