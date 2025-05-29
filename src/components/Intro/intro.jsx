cc2b71b0e2dcc2387fc3a2104e05f8ed5be9b1ab
plumbz <plumby.eschenbacher@yahoo.com>
2025-05-30 00:10:07 +0200

    styled up the intro

--- src/components/Intro/intro.jsx
+++ src/components/Intro/intro.jsx
@@ -1,10 +1,21 @@
 import React from 'react';
 import './intro.css';
+import bg from '../../assets/bg.png';
+import btnImg from '../../assets/hireMe.png'
+import { Link } from 'react-scroll';
 
-const intro = () => {
+const Intro = () => {
     return (
-        <div>intro</div>
+        <section id="intro">
+            <div className="introContent">
+                <span className="hello">Hello,</span>
+                <span className="introText">I'm <span className='introName'>Plumby</span> <br />Website Developer</span>
+                <p className="introPara">I am a passionate web developer with experience in creating resposive and user-friendly website. </p>
+           
+            </div>
+            <img src={bg} alt="Profile" className='bg' />
+        </section>
     )
 }
 
-export default intro
+export default Intro;
