26ca00883b6c4cd648bc27be4bc0a69e1e50546b
plumbz <plumby.eschenbacher@yahoo.com>
2025-05-31 20:08:38 +0200

    install emailJs, added functions to the contact and css, added footer

--- src/components/Contact/contact.jsx
+++ src/components/Contact/contact.jsx
@@ -4,8 +4,30 @@ import CaseStudy from '../../assets/CaseStudy.png';
 import Facebook from '../../assets/fb.logo.png';
 import Yahoo from '../../assets/yahoo.logo.png';
 import Github from '../../assets/github.logo.png';
+import emailjs from '@emailjs/browser';
+import { useRef } from 'react';
+
 
 const Contact = () => {
+    const form = useRef();
+     const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs
+      .sendForm('service_n967dwo', 'template_sjvwt5s', form.current, {
+        publicKey: 'tIsVlAGGT9TE4TJBZ',
+      })
+      .then(
+        () => {
+          console.log('SUCCESS!');
+          alert('Email Sent!');
+          e.target.reset();
+        },
+        (error) => {
+          console.log('FAILED...', error.text);
+        },
+      );
+    };
     return (
        <section id="contactPage">
             <div id="CaseStudy">
@@ -17,21 +39,22 @@ const Contact = () => {
             <div className='CaseImgs'>
                 <img src={CaseStudy} alt="CaseStudy" className="CaseImg"/>
             </div>
-                <div id="Contact">
-                    <h1 className="contactTitle">Contact Me</h1>
-                    <span className="contactDesc">Please fill out the form below to discuss any work opportunities.</span>
-                    <form className='contactForm'>
-                        <input type='text' className="name" placeholder='Your Name'/>
-                        <input type='email' className='email' placeholder='Your Email' />
-                        <textarea className='msg' name='message' rows="5" placeholder='Your Message'></textarea>
-                        <button type="submit" value="Send" className="submitBtn">Submit</button>
-                        <div className='links'>
-                            <img src={Facebook} alt="Facebook" className='link'/>
-                            <img src={Yahoo} alt="Yahoo" className='link'/>
-                            <img src={Github} alt="Github" className='link'/>
-                        </div> 
-                    </form>
-                </div>  
+             <button className="workBtn">See More</button>   
+            <div id="contact">
+                <h1 className="contactTitle">Contact Me</h1>
+                <span className="contactDesc">Please fill out the form below to discuss any work opportunities.</span>
+                <form className='contactForm' ref={form} onSubmit={sendEmail}>
+                    <input type='text' className="name" placeholder='Your Name' name='your_name'/>
+                    <input type='email' className='email' placeholder='Your Email' name='your_email' />
+                    <textarea className='msg' name='message' rows="5" placeholder='Your Message'></textarea>
+                    <button type="submit" value="Send" className="submitBtn">Submit</button>
+                    <div className='linksContainer'>
+                        <img src={Facebook} alt="Facebook" className='link'/>
+                        <img src={Yahoo} alt="Yahoo" className='link'/>
+                        <img src={Github} alt="Github" className='link'/>
+                    </div> 
+                </form>
+            </div>  
         </section>
     )
 }
--- src/components/Navbar/navbar.jsx
+++ src/components/Navbar/navbar.jsx
@@ -1,21 +1,23 @@
 import React from 'react';
 import './navbar.css';
 import logo from '../../assets/logo.png';
-
 import { Link } from 'react-scroll';
+
 
 const Navbar = () => {
     return (
     <nav className='navbar'>
         <img src= {logo} alt="Logo" className='logo'/>
         <div className="desktopMenu">
-            <Link className="desktopMenuListItem">Home</Link>
-            <Link className="desktopMenuListItem">About</Link>
-            <Link className="desktopMenuListItem">Portfolio</Link>
-            <Link className="desktopMenuListItem">Projects</Link>
+            <Link activeClass="active" to="intro" spy={true} smooth={true} offset={-100} duration={500} className="desktopMenuListItem">Home</Link>
+            <Link activeClass="active" to="skills" spy={true} smooth={true} offset={-100} duration={500} className="desktopMenuListItem">About</Link>
+            <Link activeClass="active" to="works" spy={true} smooth={true} offset={-100} duration={500} className="desktopMenuListItem">Portfolio</Link>
+            <Link activeClass="active" to="CaseStudy" spy={true} smooth={true} offset={-100} duration={500} className="desktopMenuListItem">CaseStudy</Link>
         
         </div>
-        <button className='desktopMenuBtn'>
+        <button className='desktopMenuBtn'onClick={() => {
+            document.getElementById('contact').scrollIntoView({behavior: 'smooth'});
+        }}>
             <img src= "" alt="" className='desktopMenuImg' /> Contact Me</button>
         
     </nav>
