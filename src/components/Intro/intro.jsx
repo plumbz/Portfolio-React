3615ff0e56170701cfb1bf203820161be1d9b309
plumbz <plumby.eschenbacher@yahoo.com>
2025-10-24 14:57:57 +0200

    changed the intro into lime green and change the picture

--- src/components/Intro/intro.jsx
+++ src/components/Intro/intro.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import './intro.css';
-import bg from '../../assets/bg.jpg';
+import pp from '../../assets/pp.png';
 import { Link } from 'react-scroll';
 
 const Intro = () => {
@@ -9,9 +9,29 @@ const Intro = () => {
             <div className="introContent">
                 <span className="hello">Hello,</span>
                 <span className="introText">I'm <span className='introName'>Plumby</span> <br />Website Developer</span>
-                <p className="introPara">I am a passionate web developer with experience in creating <br/>resposive and user-friendly websites. </p>
+                <p className="introPara">
+                    I am a passionate web developer with experience in creating <br/>
+                    responsive and user-friendly websites.
+                </p>
+                <Link 
+                    to="projects" 
+                    smooth={true} 
+                    duration={500}
+                    className="scrollBtn"
+                >
+                    <svg 
+                        width="40" 
+                        height="40" 
+                        viewBox="0 0 24 24" 
+                        fill="none" 
+                        stroke="currentColor" 
+                        strokeWidth="2"
+                    >
+                        <path d="M12 5v14M19 12l-7 7-7-7"/>
+                    </svg>
+                </Link>
             </div>
-            <img src={bg} alt="Profile" className='bg2' />
+            <img src={pp} alt="Profile" className='pp' />
         </section>
     )
 }
