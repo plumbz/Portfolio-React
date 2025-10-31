37d8974c26b417e4042cb7aea399d9307c70526d
plumbz <plumby.eschenbacher@yahoo.com>
2025-10-31 10:02:04 +0100

    changes

--- src/components/Contact/contact.jsx
+++ src/components/Contact/contact.jsx
@@ -1,14 +1,14 @@
-import React from 'react';
+import React, { useRef } from 'react';
 import './Contact.css';
 import Facebook from '../../assets/fb.logo.png';
 import Yahoo from '../../assets/yahoo.logo.png';
 import Github from '../../assets/github.logo.png';
 import emailjs from '@emailjs/browser';
-import { useRef } from 'react';
 
 const Contact = () => {
-    const form = useRef();
-     const sendEmail = (e) => {
+  const form = useRef();
+
+  const sendEmail = (e) => {
     e.preventDefault();
 
     emailjs
@@ -25,27 +25,57 @@ const Contact = () => {
           console.log('FAILED...', error.text);
         },
       );
-    };
-    return (
-       <section id="contactPage">
-          
-            <div id="contact">
-                <h1 className="contactTitle">Contact Me</h1>
-                <span className="contactDesc">Please fill out the form below to discuss any work opportunities.</span>
-                <form className='contactForm' ref={form} onSubmit={sendEmail}>
-                    <input type='text' className="name" placeholder='Your Name' name='your_name'/>
-                    <input type='email' className='email' placeholder='Your Email' name='your_email' />
-                    <textarea className='msg' name='message' rows="5" placeholder='Your Message'></textarea>
-                    <button type="submit" value="Send" className="btn">Submit</button>
-                    <div className='linksContainer'>
-                        <img src={Facebook} alt="Facebook" className='link'/>
-                        <img src={Yahoo} alt="Yahoo" className='link'/>
-                        <img src={Github} alt="Github" className='link'/>
-                    </div> 
-                </form>
-            </div>  
-        </section>
-    )
-}
+  };
 
-export default Contact
+  return (
+    <section id="contactPage">
+      <div className="contactContainer">
+        <div className="contactLeft">
+          <h1 className="contactTitle">Contact me</h1>
+          <p className="contactText">
+            Please fill out the form on the right to discuss any work opportunities.
+          </p>
+        </div>
+
+        <div className="contactRight">
+          <form ref={form} onSubmit={sendEmail} className="contactForm">
+            <div className="formRow">
+              <div className="formGroup">
+                <label>First name*</label>
+                <input type="text" name="first_name" required />
+              </div>
+              <div className="formGroup">
+                <label>Last name*</label>
+                <input type="text" name="last_name" required />
+              </div>
+            </div>
+
+            <div className="formGroup">
+              <label>E-Mail*</label>
+              <input type="email" name="your_email" required />
+            </div>
+
+            <div className="formGroup">
+              <label>Write a message</label>
+              <textarea name="message" rows="4"></textarea>
+            </div>
+
+            <button type="submit" className="submitBtn">Submit</button>
+
+            <div className="footerLinks">
+              <a href="#"><img src={Facebook} alt="Facebook" /></a>
+              <a href="#"><img src={Github} alt="Github" /></a>
+              <a href="#"><img src={Yahoo} alt="Yahoo" /></a>
+            </div>
+
+            <div className="legal">
+              <a href="#">Impressum</a> | <a href="#">Datenschutz</a>
+            </div>
+          </form>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Contact;
